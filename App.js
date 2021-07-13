d4e57ec6c21089bf22c4e6760ef8c12d2f27db3e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 23:29:42 +0530

    add a render function inside the App class

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  render () {
+    
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
