3503af572f11e12d6a1d45236b515efa5b9b6110
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 23:39:38 +0530

    add a Text tag inside the View tag

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
         <StatusBar style="auto" />
 
         <View style = {styles.header}>
-
+          <Text></Text>
         </View>
       </View>
     );
