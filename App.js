d54fd94abc7f0f70ba808cb754179f1ec4921fc4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 23:29:18 +0530

    replace the function component with the class Component App inside which extends the React.Components class

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
