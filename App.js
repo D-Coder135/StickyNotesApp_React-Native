73e14098bc6ae055b137e3cab51b3da21a861f1e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 23:42:50 +0530

    import the TextInput component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render () {
