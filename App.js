b786c33a415a11efed1b751ce97bb86886536a2d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-14 13:54:58 +0530

    pass the index field to the task property for the Node tag

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ export default class App extends React.Component {
   render () {
     var notes = this.state.notesArray.map((index, item) => {
       return (
-        <Node task = {}/>
+        <Node task = {index}/>
       );
       // return <View>
       //   <Text>{item.note}</Text>
