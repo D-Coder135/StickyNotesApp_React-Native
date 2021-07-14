99a8666d1227ef988efddcd1f1ed2be43839ed76
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-14 11:48:46 +0530

    add the onChangeText property for the TextInput tag and call the setState() function inside it

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default class App extends React.Component {
         </View>
 
         <View style = {styles.fotter}>
-          <TextInput placeholder = "Note It Down Here." style = {styles.inputText}></TextInput>
+          <TextInput placeholder = "Note It Down Here." style = {styles.inputText} onChangeText = {(text) => {this.setState({noteText:text})}}></TextInput>
 
           <TouchableOpacity style = {styles.addButton} onPress = {this.keepNotes}>
             <Text style = {{fontSize: 25}}> + </Text>
