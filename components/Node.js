59c307406fb98f251a9f155a08ea9967378374b7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-14 13:35:53 +0530

    import the required components from the react-native library

--- components/Node.js
+++ components/Node.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default class Node extends React.Component {
     render () {
