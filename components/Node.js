9b7e457464a6d46d58d034fb65bcc3ed73ff8231
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-14 13:37:35 +0530

    add one more Text tag inside the View tag

--- components/Node.js
+++ components/Node.js
@@ -6,6 +6,7 @@ export default class Node extends React.Component {
         return (
             <View style = {styles.node}>
                 <Text></Text>
+                <Text></Text>
             </View>
         );
     }
