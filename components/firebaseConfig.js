18b9a25fae3575e1394c16982b2c6450699eeca3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-14 20:52:52 +0530

    paste the code for the firebase inside the file firebaseConfig,js

--- components/firebaseConfig.js
+++ components/firebaseConfig.js
@@ -1,1 +1,13 @@
 import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBhLEcffwTafm1RN7faDfYPNuWyEt0zgp0",
+    authDomain: "stickynotesapp-29831.firebaseapp.com",
+    databaseURL: "https://stickynotesapp-29831-default-rtdb.firebaseio.com",
+    projectId: "stickynotesapp-29831",
+    storageBucket: "stickynotesapp-29831.appspot.com",
+    messagingSenderId: "1051316432823",
+    appId: "1:1051316432823:web:c1703d1f27d5dc14474232"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
