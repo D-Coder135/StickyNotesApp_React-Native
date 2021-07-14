e581917ae1c64acb3d061231ffac34dbe3a3eebd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-14 13:47:01 +0530

    import the Node.js inside the App.js file

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Node from './components/Node';
 
 export default class App extends React.Component {
 
