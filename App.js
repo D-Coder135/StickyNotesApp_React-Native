5e8633139ac81b18a03fbe1e42f43f66e4afaf3d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-15 09:26:37 +0530

    add the paddingTop property for the container attribute of the const styles field. Final Commit!

--- App.js
+++ App.js
@@ -112,6 +112,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Constants.statusBarHeight,
   },
 
   header: {
